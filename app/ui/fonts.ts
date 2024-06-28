3af85605e019d8e4be9db3bf3efd9398dd9e67ce
Benny Chen <benny.jw.chen@gmail.com>
2024-06-28 10:20:48 -0700

    add lusitana

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,8 @@
-import { Inter, Comic_Neue } from 'next/font/google';
+import { Inter, Comic_Neue, Lusitana } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
 export const comic_neue = Comic_Neue({weight: "400", preload: false});
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
